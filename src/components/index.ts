eacfa97d17025dd2a5c67907ae5ce05f9f65c326
malo-cayocca <=>
2020-10-20 17:13:25 +0200

    Ca devrait pas marcher

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Image } from './Image/Image';
+export { default as Image } from "./Image/Image";
 export { default as SearchBar } from './SearchBar/SearchBar';
 export { default as RestaurantCard } from './RestaurantCard/RestaurantCard';
 export { default as Loader } from './Loader/Loader';
